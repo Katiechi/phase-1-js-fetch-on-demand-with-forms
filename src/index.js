6e566dc96bb6080c3eff4ebc3d18874f0f624c82
brian ambundo <brian.katiechi9@gmail.com>
2022-06-15 13:00:40 +0300

    commit tree 88461e2c2d5bf5f61836ce308d272540cee51b02

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+       const input = event.target.children[1].value;
+       console.log(input)
+
+    return fetch(`http://localhost:3000/movies`)
+    .then(response => response.json()
+    )
+    .then(data =>{
+      for(let obj of data){
+          if(input == obj['id']){
+              console.log(input)
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = obj.title;
+        summary.innerText = obj.summary; 
+          }
+          
+      }
+    })
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
